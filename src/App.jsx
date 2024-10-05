99ca5f62c564b8e15c793ed5bde50ee1ce35a7c0
Mahsa <mahsaadlooo@gmail.com>
2024-10-05 14:26:36 -0700

    feat: :art: Seperate all contents into Navbar and ButtonContent.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import { ButtonContent, Navbar } from "./components";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState("");
+
+  const handleSelect = (selectedButton) => {
+    setSelectedTopic(selectedButton);
+    console.log(selectedButton);
+  };
+
   return (
     <>
-      <Navbar />
+      <Navbar onSelectButton={handleSelect} selectedTopic={selectedTopic} />
+      <ButtonContent selectedTopic={selectedTopic} />
     </>
   );
 }
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const TabButton = ({ children, onSelect }) => {
-  return <button onClick={onSelect}>{children}</button>;
+const TabButton = ({ children, onSelect, isSelected }) => {
+  return (
+    <button className={isSelected ? "active" : ""} onClick={onSelect}>
+      {children}
+    </button>
+  );
 };
 
 export default TabButton;
